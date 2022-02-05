939c3eb24c0a4bc1336c848903405360761fd726
Gutscs <gutscs@gmail.com>
2022-02-05 15:20:55 -0300

    :sparkles: Added api handler and rossmann class

---
+++ api/handler.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+import pickle
+import os
+
+from rossmann.Rossmann import Rossmann
+from flask             import Flask, request, Response
+
+
+# loading model
+home_path = os.path.abspath('')
+model = pickle.load( open( os.path.join(home_path, 'model/model_rossmann.pickle'), 'rb' ) )
+
+# Initialize API
+app = Flask( __name__ )
+
+@app.route( '/rossmann/predict', methods=['POST'] )
+def rossmann_predict():
+    test_json = request.get_json()
+
+    if test_json: # there is data
+        if isinstance( test_json, dict ):  # unique example
+            test_raw = pd.DataFrame( test_json, index=[0] )
+        else:                              # multiple examples 
+            test_raw = pd.DataFrame( test_json, columns=test_json[0].keys() )
+        
+        # Instantiate Rossmann class
+        pipeline = Rossmann()
+
+        # data cleaning
+        df1 = pipeline.data_cleaning( test_raw )
+
+        # feature engineering
+        df2 = pipeline.feature_engineering( df1 )
+
+        # data preparation
+        df3 = pipeline.data_preparation( df2 )
+
+        # prediction
+        df_response = pipeline.get_prediction( model, test_raw, df3 )
+
+        return df_response
+
+
+    else:
+        return Response( '{}', status=200, mimetype='application/json' )
+
+if __name__ == '__main__':
+    app.run( host="localhost", port=5000 )
